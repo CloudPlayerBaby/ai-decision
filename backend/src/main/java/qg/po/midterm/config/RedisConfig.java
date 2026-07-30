0f9587557ba044af8be66d173a4b0857378dc34b
Hiiii113 <3322858298@qq.com>
2026-07-30 21:45:05 +0800

    改错了一个东西，紧急修复下

--- backend/src/main/java/qg/po/midterm/config/RedisConfig.java
+++ backend/src/main/java/qg/po/midterm/config/RedisConfig.java
@@ -9,11 +9,6 @@ import org.redisson.config.SingleServerConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.serializer.RedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 /**
  * Redis 配置。
