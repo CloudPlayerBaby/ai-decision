4e210137bb963fa551e794cfeb2f52cbabfa60f5
Choco-emmm <renpei233@gmail.com>
2026-07-31 21:28:25 +0800

    fix: switch to native sentry-spring-7 SDK for Spring Boot 4.1.0 compatibility

--- backend/src/main/java/qg/po/midterm/MidTermApplication.java
+++ backend/src/main/java/qg/po/midterm/MidTermApplication.java
@@ -3,7 +3,12 @@ package qg.po.midterm;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.sentry.spring7.EnableSentry;
 
+@EnableSentry(
+    dsn = "https://e46378eb75244b37876c1adcfe6990fd@o4511829869199360.ingest.us.sentry.io/4511830110306304",
+    sendDefaultPii = true
+)
 @SpringBootApplication
 @MapperScan("qg.po.midterm.mapper")
 public class MidTermApplication {
